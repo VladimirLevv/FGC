35270d86570aa0999f29e150380d729e688d630d
vldmr_lev <vl.vl.levchenko@gmail.com>
2020-10-18 23:37:52 +0300

    new content

---
+++ js/reflink.js
@@ -0,0 +1,4 @@
+var anchors = document.querySelectorAll('a[id*="reflink"]');
+Array.prototype.forEach.call(anchors, function (element, index) {
+	element.href = "https://deckaffiliates.com/c/407689";
+});
